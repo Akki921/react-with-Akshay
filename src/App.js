574b33a9e515148fdb94f019d8b0cab4c1e1d62b
Akshay Talole <akshayclg36@gmail.com>
2021-08-18 10:21:09 +0530

    Code-with-ak

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Navbar from './copmponets/Navbar';
+function App() {
+  return (
+    <>
+      <Navbar Title="TextUtil" About="About Us" Home="HOME"/>
+</>
+  );
+}
+
+export default App;
